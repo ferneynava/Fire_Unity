951a8101a887ec669fb8c3b13861686e5b9a7a71
FerneyNava <ferneynava@gmail.com>
2019-05-26 20:35:00 -0500

    Player Mov_Camara y Mov_Player

---
+++ Assets/Script/First Person Controller/Mov_Camara.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mov_Camara : MonoBehaviour
+{
+[SerializeField] public string Inputmousex,Inputmousey;
+[SerializeField] public float sensibilidadx = 2f;
+[SerializeField] public float sensibilidady = 2f;
+[SerializeField] public Transform Player;
+ private float mousex;
+ private float mousey;
+ private float xAxis;
+
+    // Start is called before the first frame update
+    public void Awake() {
+        
+        cursor();
+        xAxis = 0.0f;
+    }
+
+    public void cursor(){
+ 
+      Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        mousex = Input.GetAxis(Inputmousex) * sensibilidadx;
+        mousey = Input.GetAxis(Inputmousey) * sensibilidady;
+        xAxis += mousey;
+
+if (xAxis > 90.0f){
+    xAxis = 90.0f;
+    mousey = 0.0f;
+    movimientoenXCam(270.0f);
+}
+else if (xAxis < -90.0f){
+    xAxis = -90.0f;
+    mousey = 0.0f;
+     movimientoenXCam(90.0f);
+}
+        transform.Rotate(Vector3.left * mousey);
+        Player.Rotate(Vector3.up * mousex);
+         
+
+    }
+
+     private void movimientoenXCam(float valorx){
+          
+         Vector3 Rotacion = transform.eulerAngles;
+          Rotacion.x = valorx;
+          transform.eulerAngles = Rotacion;
+
+}
+}
---
+++ Assets/Script/First Person Controller/Mov_Player.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mov_Player : MonoBehaviour
+{
+[SerializeField] public string InputHorizaontal, InputVertical;
+[SerializeField] public float speed;
+    private float Vertical;
+    private float Horizontal;
+[SerializeField] private float Gravity = 9.8f;
+    private float VelocidadCaida; 
+
+[SerializeField] public CharacterController player;
+ [SerializeField] public Camera Cam;
+    private Vector3 MovPlayer;
+    private Vector3 CamDelante;
+    private Vector3 CamDerecha;
+    private Vector3 Mov;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+           Horizontal = Input.GetAxis(InputHorizaontal);
+           Vertical = Input.GetAxis(InputVertical);
+
+           Mov = new Vector3 (Horizontal, 0.0f, Vertical);
+           Mov = Vector3.ClampMagnitude(Mov, 1);
+
+           Camdirection();
+
+           MovPlayer = Mov.x * CamDerecha + Mov.z * CamDelante;
+
+           MovPlayer = MovPlayer * speed;
+
+           setGravity();
+
+           player.Move(MovPlayer * Time.deltaTime);
+
+
+    }
+
+    void Camdirection(){
+
+     CamDelante = Cam.transform.forward; // Guardar la direccion hacia adelante de la camara principal 
+     CamDerecha = Cam.transform.right;
+
+     CamDelante.y = 0;
+     CamDerecha.y = 0;
+
+     CamDelante = CamDelante.normalized; // Valor normalizado de transform.forward
+     CamDerecha = CamDerecha.normalized; // Valor normalizado de transform.right
+    }
+
+    void setGravity(){
+
+        
+        if(player.isGrounded){ //si el Jugador esta Tocando el suelo 
+        VelocidadCaida = -Gravity * Time.deltaTime;
+        MovPlayer.y = VelocidadCaida;
+        } 
+        else{
+        VelocidadCaida -=  Gravity * Time.deltaTime;
+        MovPlayer.y = VelocidadCaida;
+        }
+    }
+}
